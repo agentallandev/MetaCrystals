6856b2c3e08a995d06ca3269156680f60e04276c
agentallandev <60478734+agentallandev@users.noreply.github.com>
2022-08-16 22:12:07 -0400

    MCreator Merge commit '8dfda7c4b9278b950385fa1940e2ec6c4bba4362' into 1e3967f1eefd4ae20bcfcd6de898e99f3f3c26f5

--- src/main/java/com/github/agentallandev/metacrystals/init/MetacrystalsModItems.java
+++ src/main/java/com/github/agentallandev/metacrystals/init/MetacrystalsModItems.java
@@ -14,6 +14,11 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
 import com.github.agentallandev.metacrystals.item.Tier6WatchItem;
+import com.github.agentallandev.metacrystals.item.Tier5WatchItem;
+import com.github.agentallandev.metacrystals.item.Tier4WatchItem;
+import com.github.agentallandev.metacrystals.item.Tier3WatchItem;
+import com.github.agentallandev.metacrystals.item.Tier2WatchItem;
+import com.github.agentallandev.metacrystals.item.Tier1WatchItem;
 import com.github.agentallandev.metacrystals.item.PowerCrystalItem;
 import com.github.agentallandev.metacrystals.item.MagicCrystalItem;
 import com.github.agentallandev.metacrystals.item.HasteCrystalItem;
@@ -35,6 +40,11 @@ public class MetacrystalsModItems {
 	public static final RegistryObject<Item> AGILITY_CRYSTAL = REGISTRY.register("agility_crystal", () -> new AgilityCrystalItem());
 	public static final RegistryObject<Item> POWER_CRYSTAL = REGISTRY.register("power_crystal", () -> new PowerCrystalItem());
 	public static final RegistryObject<Item> TIER_6_WATCH = REGISTRY.register("tier_6_watch", () -> new Tier6WatchItem());
+	public static final RegistryObject<Item> TIER_5_WATCH = REGISTRY.register("tier_5_watch", () -> new Tier5WatchItem());
+	public static final RegistryObject<Item> TIER_4_WATCH = REGISTRY.register("tier_4_watch", () -> new Tier4WatchItem());
+	public static final RegistryObject<Item> TIER_3_WATCH = REGISTRY.register("tier_3_watch", () -> new Tier3WatchItem());
+	public static final RegistryObject<Item> TIER_2_WATCH = REGISTRY.register("tier_2_watch", () -> new Tier2WatchItem());
+	public static final RegistryObject<Item> TIER_1_WATCH = REGISTRY.register("tier_1_watch", () -> new Tier1WatchItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
